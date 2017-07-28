deef98c8ba1dfaf005167c2aaf3d7170717047d4
John Yorke <yorkejohn@yahoo.com>
2017-07-27 23:13:05 -0300

    Update version info

--- src/main/java/taintedmagic/common/lib/LibInfo.java
+++ src/main/java/taintedmagic/common/lib/LibInfo.java
@@ -4,6 +4,6 @@ public class LibInfo
 {
 	public static final String MODID = "TaintedMagic";
 	public static final String NAME = "Tainted Magic";
-	public static final String VERSION = "r7.7";
+	public static final String VERSION = "r7.8";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);required-after:Thaumcraft@[4.2.3.5,);after:ThaumicTinkerer";
 }
