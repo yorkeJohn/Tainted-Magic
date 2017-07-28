1ebcb105a64f2c82d05093f17ca2f1966d011952
John Yorke <yorkejohn@yahoo.com>
2017-07-27 23:11:53 -0300

    Bugfixes

--- src/main/java/taintedmagic/common/items/wand/foci/FocusUpgrades.java
+++ src/main/java/taintedmagic/common/items/wand/foci/FocusUpgrades.java
@@ -7,9 +7,9 @@ import thaumcraft.api.wands.FocusUpgradeType;
 
 public class FocusUpgrades
 {
-	public static int ID = Short.MAX_VALUE;
+	public static int ID = Short.MAX_VALUE - 5;
 	public static FocusUpgradeType sanity = new FocusUpgradeType(ID--, new ResourceLocation("taintedmagic:textures/foci/IconSanity.png"), "focus.upgrade.sanity.name", "focus.upgrade.sanity.text", new AspectList().add(Aspect.MIND, 1).add(Aspect.HEAL, 1));
-	public static FocusUpgradeType bloodlust = new FocusUpgradeType(ID--, new ResourceLocation("taintedmagic:textures/misc/IconBloodlust.png"), "focus.upgrade.bloodlust.name", "focus.upgrade.bloodlust.text", new AspectList().add(Aspect.WEAPON, 1).add(Aspect.HEAL, 1));
+	public static FocusUpgradeType bloodlust = new FocusUpgradeType(ID--, new ResourceLocation("taintedmagic:textures/foci/IconBloodlust.png"), "focus.upgrade.bloodlust.name", "focus.upgrade.bloodlust.text", new AspectList().add(Aspect.WEAPON, 1).add(Aspect.HEAL, 1));
 	public static FocusUpgradeType antibody = new FocusUpgradeType(ID--, new ResourceLocation("taintedmagic:textures/foci/IconAntibody.png"), "focus.upgrade.antibody.name", "focus.upgrade.antibody.text", new AspectList().add(Aspect.TAINT, 1).add(Aspect.HEAL, 1));
 	public static FocusUpgradeType corrosive = new FocusUpgradeType(ID--, new ResourceLocation("taintedmagic:textures/foci/IconCorrosive.png"), "focus.upgrade.corrosive.name", "focus.upgrade.corrosive.text", new AspectList().add(Aspect.TAINT, 1).add(Aspect.POISON, 1));
 	public static FocusUpgradeType persistent = new FocusUpgradeType(ID--, new ResourceLocation("taintedmagic:textures/foci/IconPersistent.png"), "focus.upgrade.persistent.name", "focus.upgrade.persistent.text", new AspectList().add(Aspect.ARMOR, 1).add(Aspect.MOTION, 1).add(Aspect.ENERGY, 1));
