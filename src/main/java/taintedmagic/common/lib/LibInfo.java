cd59aac086aa06562b08e4b577b585d51c55663b
John Yorke <yorkejohn02@gmail.com>
2021-11-28 13:00:55 -0400

    Bump version to r8.1

--- src/main/java/taintedmagic/common/lib/LibInfo.java
+++ src/main/java/taintedmagic/common/lib/LibInfo.java
@@ -4,7 +4,7 @@ public class LibInfo {
 
     public static final String MODID = "TaintedMagic";
     public static final String NAME = "Tainted Magic";
-    public static final String VERSION = "r8.0";
+    public static final String VERSION = "r8.1";
     public static final String DEPENDENCIES =
             "required-after:Forge@[10.13.4.1448,);required-after:Thaumcraft@[4.2.3.5,);after:ThaumicTinkerer";
 }
